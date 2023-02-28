5f3682cb18af47810b42a6afb3ac82dbd7abefd4
AndreiCherniaev <glscene2010@gmail.com>
2023-02-28 15:39:25 +0900

    add ctrl+c SigInt router
    from console
    ^Cinterrupted with code 2

--- main.cpp
+++ main.cpp
@@ -1,10 +1,18 @@
 #include <QCoreApplication>
+#include <signal.h>
 #include "rial.h"
+
+void SigInt_Handler(int n_signal){
+    printf("interrupted with code %u\n", n_signal); //Ctrl+C
+    exit(1);
+}
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
     rial s(QCoreApplication::instance());
+
+    signal(SIGINT, &SigInt_Handler);
 
     int ret= QCoreApplication::exec();
     return ret;
