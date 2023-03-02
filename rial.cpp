7459ad7948d5e62cf40f8020b4916e35cd502def
AndreiCherniaev <glscene2010@gmail.com>
2023-03-02 10:11:02 +0900

    change exit method
    For more control in case of Ctrl+C see
    https://forum.qt.io/topic/5927/how-to-use-sigaction-in-qt/6
    https://stackoverflow.com/questions/54467652/how-to-set-sa-handlerint-pointer-to-function-which-is-member-of-a-class-i
    https://web.archive.org/web/20101004072608/http://doc.qt.nokia.com/4.7/unix-signals.html

--- rial.cpp
+++ rial.cpp
@@ -3,7 +3,7 @@
 rial::rial(QObject *parent)
     : QObject(parent)
 {
-    QTimer::singleShot(1000, this, &rial::closeApp); //if you need process some code in closeApp()
+    QTimer::singleShot(3000, this, &rial::closeApp); //if you need process some code in closeApp()
     //QTimer::singleShot(1000, QCoreApplication::instance(), SLOT(quit())); //if you need quit() only
 }
 
